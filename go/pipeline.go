7028bea8bc0c6e8ef7524be2e346ca276acc04b8
feliciavs24 <feso@itu.dk>
2025-11-23 15:50:51 +0100

    go pipeline: comments

--- go/pipeline.go
+++ go/pipeline.go
@@ -20,19 +20,23 @@ func main() {
 
 func Build(ctx context.Context) error {
 	// Initialize Dagger client
-	client, err := dagger.Connect(ctx)
+	client, err := dagger.Connect(ctx) // Connects to dagger engine
 	if err != nil {
 		return err
 	}
 	defer client.Close()
 
+	// Pulls container from docker
 	python := client.Container().From("python:3.12.2-bookworm").
+		// copies local local "python-files" folder into container at path "python"
 		WithDirectory("python", client.Host().Directory("python-files")).
+		// executes python script inside the container
 		WithExec([]string{"python", "--version"})
 
 	python = python.WithExec([]string{"python", "python/hello.py"})
 
 	_, err = python.
+		// exports container's "output" folder back to host filesystem (GitHub actions)
 		Directory("output").
 		Export(ctx, "output")
 	if err != nil {
